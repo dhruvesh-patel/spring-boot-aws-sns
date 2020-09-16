c2bb448c6a765bc3fe8ac117c9596fd5ffcc23cd
Dhruvesh Patel <dmpatel.1412@gmail.com>
2020-09-16 21:25:40 +0100

    Added SNS spring boot project

---
+++ spring-boot-aws-sns/src/main/java/com/dpins/springbootawssns/SpringBootAwsSnsApplication.java
@@ -0,0 +1,13 @@
+package com.dpins.springbootawssns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsSnsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-aws-sns/src/test/java/com/dpins/springbootawssns/SpringBootAwsSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpins.springbootawssns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
